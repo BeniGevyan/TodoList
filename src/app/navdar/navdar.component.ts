79f9bb111aef46f6842759071a941eb012912b98
benig <cbhdchui@gmail.com>
2022-04-07 20:31:06 +0300

    Creating Todo Form and Navbar

---
+++ src/app/navdar/navdar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navdar',
+  templateUrl: './navdar.component.html',
+  styleUrls: ['./navdar.component.scss']
+})
+export class NavdarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
