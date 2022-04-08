53f59d023eae339c028d05bc384c9615e0e37137
benig <cbhdchui@gmail.com>
2022-04-08 13:10:58 +0300

    add updete but

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,8 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ITask } from '../model/task';
@@ -9,32 +13,69 @@ import { ITask } from '../model/task';
   styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent implements OnInit {
- 
-  todoForm !: FormGroup
+  todoForm!: FormGroup;
   tasks: ITask[] = [];
   inprogress: ITask[] = [];
   done: ITask[] = [];
-  
+  updeteIndex!: any;
+  isEditEnabled: boolean = false;
+
   constructor(private fb: FormBuilder) {}
 
-  
   ngOnInit(): void {
     this.todoForm = this.fb.group({
       item: ['', Validators.required],
     });
   }
 
+  addTask() {
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done: false,
+    });
+    this.todoForm.reset();
+ 
+  }
+
+  onEdit(item: ITask, i: number) {
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updeteIndex = i;
+    this.isEditEnabled = true;
+  }
+
+  updeteTask() {
+    this.tasks[this.updeteIndex].description = this.todoForm.value.item;
+    this.tasks[this.updeteIndex].done = false;
+    this.todoForm.reset();
+    this.updeteIndex = undefined;
+    this.isEditEnabled = false;
+  }
+
+  deleteTask(i: number) {
+    this.tasks.splice(i, 1);
+  }
+
+  deleteInPogressTask(i: number) {
+    this.inprogress.splice(i, 1);
+  }
+  deleteDoneTask(i: number) {
+    this.done.splice(i, 1);
+  }
+
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
-  
 }
